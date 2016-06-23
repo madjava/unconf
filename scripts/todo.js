5acaf5b33b57793941c1a2c95ebc4c83d9ed9f7f
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2016-06-23 15:48:36 +0100

    Update to test cases for exercise purposes

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,19 +2,40 @@
 
 	var Todo = function(owner) {
 		this.owner = owner || 'Unknown';
+		this.days = [];
 	};
 
 	// Ex. 1
 	// Todo.prototype.getOwner = function() {
 	// 	return this.owner;
 	// };
-
+ 
 	//Ex. 2
 	// Todo.prototype.setOwner = function(owner) {
 	// 	this.owner = owner;
 	// }
 
+    //Ex. 3
+	Todo.prototype.fetchDays = function() {
+		var _self = this;
+		$.ajax({
+			url: '/data/days.json',
+			dataType: 'json',
+			success: function(data) {
+				_self.setDays(data);
+			}
+		});
+	};
+
+	Todo.prototype.setDays = function(data) {
+		this.days = data.days;
+	};
+
+	Todo.prototype.getDays = function(data) {
+		return this.days;
+	};
+
 
 	global.Todo = Todo;
-	
+
 }(this));
--- spec/todo-tests.js
+++ spec/todo-tests.js
@@ -6,40 +6,40 @@ var should = chai.should();
  * Example 1. Testing our Object Under Test
  */
 describe('Test that the Todo script exists', function() {
-	var todo;
-	before(function() {
-		todo = new Todo();
-	});
+	// var todo;
+	// before(function() {
+	// 	todo = new Todo();
+	// });
 
-	//1. Using expect matcher
-	it('Should have the object existing - expect', function() {
-		expect(todo).to.be.an('object');
-	});
+	// //1. Using expect matcher
+	// it('Should have the object existing - expect', function() {
+	// 	expect(todo).to.be.an('object');
+	// });
 
-	//2. Using assertion matcher
-	it('Should have the object existing - assert', function() {
-		assert.typeOf(todo, 'object', 'should be a valid Todo object');
-	});
+	// //2. Using assertion matcher
+	// it('Should have the object existing - assert', function() {
+	// 	assert.typeOf(todo, 'object', 'should be a valid Todo object');
+	// });
 
-	//3. Using should matcher
-	it('Should have the object existing - should', function() {
-		todo.should.be.a('object');
-	});
+	// //3. Using should matcher
+	// it('Should have the object existing - should', function() {
+	// 	todo.should.be.a('object');
+	// });
 });
- 
+
 
 /**
  * Example 2. Test our Objects core public API, ensuring its consistency
  */
 describe('Test that relevant API exist and behaves as expected', function() {
-	var todo;
-	before(function() {
-		todo = new Todo();
-	});
+	// var todo;
+	// before(function() {
+	// 	todo = new Todo();
+	// });
 
-	it('should have to owner property existing', function() {
-		expect(todo).to.have.property('owner').and.to.equal('Unknown');
-	});
+	// it('should have to owner property existing', function() {
+	// 	expect(todo).to.have.property('owner').and.to.equal('Unknown');
+	// });
 
 	//** Ex. 1a - Uncomment block, test should fail. Make test pass 
 	// it('should have its accessor methods in existence', function(){
@@ -51,8 +51,8 @@ describe('Test that relevant API exist and behaves as expected', function() {
 	//    todo = new Todo('John Doe');
 	//    expect(todo.getOwner()).to.equal('John Doe');
 	// });
-    
-    //** Ex. 2 - Uncomment block, test should fail. Make test pass 
+
+	//** Ex. 2 - Uncomment block, test should fail. Make test pass 
 	// it('should have the owner property correctly', function(){
 	//    todo = new Todo('John Doe');
 	//    todo.setOwner('Jane Doe');
@@ -61,11 +61,64 @@ describe('Test that relevant API exist and behaves as expected', function() {
 });
 
 /**
+ * Example 3. Mocking Ajax calls in unit tests
+ */
+describe('When i request for days of the week from the server', function() {
+	// 	var todo;
+	// 	beforeEach(function() {
+	// 		todo = new Todo('Jane Doe');
+	// 		sinon.stub($, 'ajax').yieldsTo('success', {
+	//         days : ["Monday"]
+	//       });
+	// 	});
+
+	// 	afterEach(function() {
+	// 		$.ajax.restore();
+	// 	});
+
+	// 	it('#fetchDays - should make a valid call to /data api', function(done) {
+	// 		todo.fetchDays();
+	// 		expect($.ajax.calledOnce).to.be.true;
+	// 		done();
+	// 	});
+
+	// 	it('#getDays - should have been set to correct values', function(done) {
+	// 		todo.fetchDays();
+	// 		expect(todo.getDays()[0]).to.equal("Monday"); 
+	// 		done();
+	// 	});
+});
+
+/**
+ * Example 4. Spies, and verifying that methods where called
+ */
+describe('When i request for days of the week from server and it returns 200', function() {
+	// var todo, spy_setDays;
+	// beforeEach(function() {
+	// 	todo = new Todo('Jane Doe');
+	// 	spy_setDays = sinon.spy(todo, 'setDays');
+	// 	sinon.stub($, 'ajax').yieldsTo('success', {
+	// 		days: ["Tuesday"]
+	// 	});
+	// });
+
+	// afterEach(function() {
+	// 	$.ajax.restore();
+	// });
+
+	// it('should call #setDays and set the correct value', function() {
+	// 	todo.fetchDays();
+	// 	expect(todo.getDays()[0]).to.equal("Tuesday");
+	// 	assert.isTrue(spy_setDays.called, '#setDays should have been called once');
+	// });
+});
+
+/**
  * Requirement: When i ask for a day in the week using a number 1 to 7
  * i expect to get the corresponding day of the week name e.g. 2 should give me Tuesday
  */
 
- /**
-  * Requirement: I want to be able to sort a list of todos for one week and view them by passing in a number
-  * for the day i want to see all the todos for that day
-  */
+/**
+ * Requirement: I want to be able to sort a list of todos for one week and view them by passing in a number
+ * for the day i want to see all the todos for that day
+ */
